6224a5e306b423543cb0c72d0be653ffefabb130
dn0rmand <dominique@normand.us>
2025-12-29 08:33:27 -0500

    quest16 done

--- src/tools/readData.h
+++ src/tools/readData.h
@@ -1,22 +1,23 @@
-#include <stdint.h>
 #include <cstdio>
 #include <cstdlib>
+#include <stdint.h>
 
 #ifndef READ_DATA_H
 #define READ_DATA_H
 
 namespace tools
 {
-    void *readData(const char day[], int part) {
+    void *readData(const char day[], int part)
+    {
         char name[100];
 
         snprintf(name, 100, "../data/quest%s/%i.raw", day, part);
         FILE *file = fopen(name, "r");
-        
+
         fseek(file, 0L, SEEK_END);
         long size = ftell(file);
 
-        char *data = (char*)malloc(size+1);
+        char *data = (char *)malloc(size + 1);
         fseek(file, 0L, SEEK_SET);
         fread(data, 1, size, file);
         fclose(file);
@@ -35,6 +36,35 @@ namespace tools
             ptr++;
         }
         return length;
+    }
+
+    int readNumber(char **data)
+    {
+        char *ptr = *data;
+        // Ignore white spaces
+        while (*ptr == ' ')
+        {
+            ptr++;
+        }
+        // Expect digit
+        if (*ptr < '0' || *ptr > '9')
+        {
+            throw;
+        }
+        // Read value
+        int value = 0;
+        while (*ptr >= '0' && *ptr <= '9')
+        {
+            value = value * 10 + (*ptr - '0');
+            ptr++;
+        }
+        // Ignore white spaces
+        while (*ptr == ' ')
+        {
+            ptr++;
+        }
+        *data = ptr;
+        return value;
     }
 }
 
